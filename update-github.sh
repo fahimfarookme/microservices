5a2c153d83e3238d1085d1868c7739024043cb08
Fahim Farook <ffahim@virtusapolaris.com>
2017-05-14 19:24:50 +0530

    Updated

--- update-github.sh
+++ update-github.sh
@@ -4,7 +4,7 @@
 echo 'Enter new value: '
 read value
 
-echo "message=$value" >> "xml-refresh-scope.properties"
+echo "message=$value" > "xml-refresh-scope.properties"
 git add *
 git commit -m "Updated"
 git push origin master
